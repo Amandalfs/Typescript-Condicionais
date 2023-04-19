3db28d81b5df89baaa52d1d17494429ec6c92531
Amandalfs <caduc57@gmail.com>
2023-04-19 20:31:26 -0300

    Exercicio 01 Concluido

--- exercicio1/exercicio1.ts
+++ exercicio1/exercicio1.ts
@@ -1,1 +1,10 @@
 //Escreve seu codigo aqui
+
+function verParOuImpar(numero:number):void{
+    if(numero%2===0){
+        console.log("O numero e PAR");
+    } else {
+        console.log("O numero e IMPAR");
+    }
+}
+verParOuImpar(10);

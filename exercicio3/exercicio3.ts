e28694ab2ce97facd34b34ebcdfb6079a5394dd4
Amandalfs <caduc57@gmail.com>
2023-04-19 20:37:14 -0300

    Exercicio 03 Concluido

--- exercicio3/exercicio3.ts
+++ exercicio3/exercicio3.ts
@@ -1,1 +1,11 @@
 //Escreve seu codigo aqui
+
+function verMaiorNumero(num1:number, num2:number): number{
+    if(num1>num2){
+        return num1
+    } else {
+        return num2
+    }
+}
+
+console.log(verMaiorNumero(50,10));

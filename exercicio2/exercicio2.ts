83f3dc07dcd29b7038f3fa19e05bcf36d7dfcc54
Amandalfs <caduc57@gmail.com>
2023-04-19 20:34:23 -0300

    Exercicio 02 Concluido

--- exercicio2/exercicio2.ts
+++ exercicio2/exercicio2.ts
@@ -1,1 +1,13 @@
 //Escreve seu codigo aqui
+
+function verPositivo(numero:number):void {
+    if(numero<0){
+        console.log("Numero e negativo")
+    } else if(numero===0){
+        console.log("Numero e igual a zero")
+    } else {
+        console.log("Numero e posivo")
+    }
+}
+
+verPositivo(-885)

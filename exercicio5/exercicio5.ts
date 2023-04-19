3b2d7de09aeb9d7b541016580c4933f87f00db0a
Amandalfs <caduc57@gmail.com>
2023-04-19 20:56:12 -0300

    Exercicio 05 Concluido

--- exercicio5/exercicio5.ts
+++ exercicio5/exercicio5.ts
@@ -1,1 +1,19 @@
 //Escreve seu codigo aqui
+
+function verPrimo(numero:number):void{
+    let vezesDiv:number = 0;
+
+    for(let indice:number = 1; indice<=numero; indice++){
+        if(numero%indice === 0){
+            vezesDiv++
+        }
+    }
+
+    if(vezesDiv==2){
+        console.log("o numero e primo");
+    } else {
+        console.log("O numero nao e primo")
+    }
+}
+
+verPrimo(3)

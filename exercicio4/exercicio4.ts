c8354c03458f351a9f28d159f441488afb166cfa
Amandalfs <caduc57@gmail.com>
2023-04-19 20:41:10 -0300

    Exercicio 04 Concluido

--- exercicio4/exercicio4.ts
+++ exercicio4/exercicio4.ts
@@ -1,1 +1,13 @@
 //Escreve seu codigo aqui
+
+function verMaior(num1:number, num2:number, num3:number):number{
+    if(num1>num2 && num1>num3){
+        return num1;
+    } else if(num2>num1 && num2>num3){
+        return num2;
+    } else {
+        return num3
+    }
+}
+
+console.log(verMaior(5,25,3))

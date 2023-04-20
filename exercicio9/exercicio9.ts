cf97131272fd150dec83c686caaf13c4cd0ec170
Amandalfs <caduc57@gmail.com>
2023-04-19 21:35:56 -0300

    Exercicio 09 Concluido

--- exercicio9/exercicio9.ts
+++ exercicio9/exercicio9.ts
@@ -1,1 +1,12 @@
 //Escreve seu codigo aqui
+
+function verQuadradoPerfeito(numero:number):void{
+    const raiz:number = Math.sqrt(numero);
+    if(Number.isInteger(raiz)){
+        console.log("Quadrado Perfeito");
+    } else {
+        console.log("quadrado Inperfeito")
+    }
+}
+
+verQuadradoPerfeito(9)

54006d7022084a8badbe36d0ff5d709937791c41
Amandalfs <caduc57@gmail.com>
2023-04-19 22:08:00 -0300

    Exercicio 10 Concluido

--- exercicio10/exercicio10.ts
+++ exercicio10/exercicio10.ts
@@ -1,1 +1,18 @@
 //Escreve seu codigo aqui
+
+function verNumero(numero:number):void{
+    const arrayDeNumeros:string [] = (String(numero).split(""))
+    let soma:number = 0;
+
+    for(let indice in arrayDeNumeros){
+            soma += Number(arrayDeNumeros[Number(indice)]) ** 3;
+    }
+    
+    if(soma===numero){
+        console.log("número de Armstrong")
+    } else {
+        console.log("nao e número de Armstrong")
+    }
+}
+
+verNumero(153)

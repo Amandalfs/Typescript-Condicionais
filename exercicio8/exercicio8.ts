8f81b3665f07fc7672420df918e671ba979f8806
Amandalfs <caduc57@gmail.com>
2023-04-19 21:21:20 -0300

    Exercicio 08 Concluido

--- exercicio8/exercicio8.ts
+++ exercicio8/exercicio8.ts
@@ -1,1 +1,31 @@
 //Escreve seu codigo aqui
+
+function verificarPalavra(palavra:string):void{
+    const arrayDePalavras:string[] = palavra.toLowerCase().split('');
+
+    const somenteVogais = arrayDePalavras.every((elemento:string)=>{
+        const vogais:string [] = ["a", "e", "i", "o", "u"]
+        if(vogais.includes(elemento)){
+            return true
+        }
+    })
+
+    const somenteConsoantes = arrayDePalavras.every((elemento:string)=>{
+        const conso:string [] = ["q", "w", "r", "t", "y", 'p', 's', 'd', 'f','g','h','j','k','l','z','c','v','b','n','m']
+        if(conso.includes(elemento)){
+            return true
+        }
+    })
+
+    if(somenteVogais){
+        console.log("a palavra tem apenas vogais.")
+    } else if(somenteConsoantes) {
+        console.log("a palavra tem apenas consoantes.")
+    } else {
+        console.log("a palavra e uma mistura de ambos.")
+    }
+
+}
+
+
+verificarPalavra("www")

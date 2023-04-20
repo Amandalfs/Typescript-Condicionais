189f6f08bb77b35747519c2121ee8b333ac32562
Amandalfs <caduc57@gmail.com>
2023-04-19 21:02:26 -0300

    Exercicio 06 Concluido

--- exercicio6/exercicio6.ts
+++ exercicio6/exercicio6.ts
@@ -1,1 +1,18 @@
 //Escreve seu codigo aqui
+
+function numeroPerfeito(numero: number):void{
+    let soma: number = 0;
+    
+    for(let i:number = 0; i<numero; i++){
+        if(numero%i===0){
+            soma += i;
+        }
+    }
+    if(soma===numero){
+        console.log("Oow numero perfeito");
+    } else {
+        console.log("Infelismente esse numero nao e perfeito")
+    }
+}
+
+numeroPerfeito(28)

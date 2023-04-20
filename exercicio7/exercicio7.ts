c49c3e21fb614744db563f3b720c7e83031877da
Amandalfs <caduc57@gmail.com>
2023-04-19 21:09:33 -0300

    Exercicio 07 Concluido

--- exercicio7/exercicio7.ts
+++ exercicio7/exercicio7.ts
@@ -1,1 +1,14 @@
 //Escreve seu codigo aqui
+
+function verPali(palavra:string): void{
+    const textoEmArray:string[] = palavra.split('');
+    const palavraInvertida:string = textoEmArray.join('')
+    
+    if(palavra===palavraInvertida){
+        console.log("e palíndromo");
+    } else {
+        console.log("nao e palíndromo")
+    }
+}
+
+verPali("ana")
